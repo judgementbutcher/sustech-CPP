b5ea541ef874ef2ee72790f8863157a432e71be8
judgementbutcher <shendiyoncun0@gmail.com>
2023-06-05 18:24:47 +0800

    the first week

---
+++ week01/lab01/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    double a = 0.1,b = 0.2, c= 0.3;
+    cout << fixed << setprecision(2) << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    return 0;
+}
